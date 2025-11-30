83090d28c111cd495a7c756351538deefa0ce64b
shawinsoranakom <shawinsoranakom@gmail.com>
2025-11-30 11:57:11 +0700

    Create Snip0017.py

---
+++ Snip0017.py
@@ -0,0 +1,26 @@
+def get_categories_content(contents: List[str]) -> Tuple[Categories, CategoriesLineNumber]:
+
+    categories = {}
+    category_line_num = {}
+
+    for line_num, line_content in enumerate(contents):
+
+        if line_content.startswith(anchor):
+            category = line_content.split(anchor)[1].strip()
+            categories[category] = []
+            category_line_num[category] = line_num
+            continue
+
+        if not line_content.startswith('|') or line_content.startswith('|---'):
+            continue
+
+        raw_title = [
+            raw_content.strip() for raw_content in line_content.split('|')[1:-1]
+        ][0]
+
+        title_match = link_re.match(raw_title)
+        if title_match:
+                title = title_match.group(1).upper()
+                categories[category].append(title)
+
+    return (categories, category_line_num)
