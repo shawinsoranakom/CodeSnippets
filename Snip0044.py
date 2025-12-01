4b190ab329b7a27b26f6b4379d3a821ffab6f3f1
Bossu284 <sirivej817@gmail.com>
2025-12-01 19:11:44 +0700

    Create Snip0044.py

---
+++ Snip0044.py
@@ -0,0 +1,7 @@
+def process_query(self, query):
+        query = self.parse_query(query)
+        results = self.memory_cache.get(query)
+        if results is None:
+            results = self.reverse_index_cluster.process_search(query)
+            self.memory_cache.set(query, results)
+        return results
