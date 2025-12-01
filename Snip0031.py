e008402e624be279b48257e70a67166eb8a46374
Bossu284 <sirivej817@gmail.com>
2025-12-01 19:02:12 +0700

    Update Snip0031.py

--- Snip0031.py
+++ Snip0031.py
@@ -1,1 +1,17 @@
+class SpendingByCategory(MRJob):
 
+    def __init__(self, categorizer):
+        self.categorizer = categorizer
+        ...
+
+    def current_year_month(self):
+        """Return the current year and month."""
+        ...
+
+    def extract_year_month(self, timestamp):
+        """Return the year and month portions of the timestamp."""
+        ...
+
+    def handle_budget_notifications(self, key, total):
+        """Call notification API if nearing or exceeded budget."""
+        ...
