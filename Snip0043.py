8045b2c0f514b9746bbf872a068627afebe0274c
Bossu284 <sirivej817@gmail.com>
2025-12-01 19:09:24 +0700

    Create Snip0043.py

---
+++ Snip0043.py
@@ -0,0 +1,5 @@
+class QueryApi(object):
+
+    def __init__(self, memory_cache, reverse_index_cluster):
+        self.memory_cache = memory_cache
+        self.reverse_index_cluster = reverse_index_cluster
