954840d83d84283516bc219c2a79e9970652f1c8
Bossu284 <sirivej817@gmail.com>
2025-12-03 11:54:39 +0700

    Create Snip0062.py

---
+++ Snip0062.py
@@ -0,0 +1,28 @@
+def make_lowshelf(
+    frequency: int,
+    samplerate: int,
+    gain_db: float,
+    q_factor: float = 1 / sqrt(2),
+) -> IIRFilter:
+  
+    w0 = tau * frequency / samplerate
+    _sin = sin(w0)
+    _cos = cos(w0)
+    alpha = _sin / (2 * q_factor)
+    big_a = 10 ** (gain_db / 40)
+    pmc = (big_a + 1) - (big_a - 1) * _cos
+    ppmc = (big_a + 1) + (big_a - 1) * _cos
+    mpc = (big_a - 1) - (big_a + 1) * _cos
+    pmpc = (big_a - 1) + (big_a + 1) * _cos
+    aa2 = 2 * sqrt(big_a) * alpha
+
+    b0 = big_a * (pmc + aa2)
+    b1 = 2 * big_a * mpc
+    b2 = big_a * (pmc - aa2)
+    a0 = ppmc + aa2
+    a1 = -2 * pmpc
+    a2 = ppmc - aa2
+
+    filt = IIRFilter(2)
+    filt.set_coefficients([a0, a1, a2], [b0, b1, b2])
+    return filt
