02a1b5f32fc04e8aa68cf579dc66a1196f4859ae
Bossu284 <sirivej817@gmail.com>
2025-12-03 11:50:03 +0700

    Create Snip0057.py

---
+++ Snip0057.py
@@ -0,0 +1,21 @@
+def make_lowpass(
+    frequency: int,
+    samplerate: int,
+    q_factor: float = 1 / sqrt(2),
+) -> IIRFilter:
+    
+    w0 = tau * frequency / samplerate
+    _sin = sin(w0)
+    _cos = cos(w0)
+    alpha = _sin / (2 * q_factor)
+
+    b0 = (1 - _cos) / 2
+    b1 = 1 - _cos
+
+    a0 = 1 + alpha
+    a1 = -2 * _cos
+    a2 = 1 - alpha
+
+    filt = IIRFilter(2)
+    filt.set_coefficients([a0, a1, a2], [b0, b1, b0])
+    return filt
