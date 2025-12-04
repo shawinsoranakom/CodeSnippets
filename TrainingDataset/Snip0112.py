826e7e012082eafdd32a67a79da8fd55135028b9
Bossu284 <sirivej817@gmail.com>
2025-12-04 08:31:11 +0700

    Create Snip0112.py

---
+++ TrainingDataset/Snip0112.py
@@ -0,0 +1,23 @@
+def create_state_space_tree(
+    nums: list[int],
+    max_sum: int,
+    num_index: int,
+    path: list[int],
+    result: list[list[int]],
+    remaining_nums_sum: int,
+) -> None:
+
+    if sum(path) > max_sum or (remaining_nums_sum + sum(path)) < max_sum:
+        return
+    if sum(path) == max_sum:
+        result.append(path)
+        return
+    for index in range(num_index, len(nums)):
+        create_state_space_tree(
+            nums,
+            max_sum,
+            index + 1,
+            [*path, nums[index]],
+            result,
+            remaining_nums_sum - nums[index],
+        )
