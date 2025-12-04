b597a5f85e44c9b351faf8b0946ed64b3b5653a8
Bossu284 <sirivej817@gmail.com>
2025-12-04 10:35:05 +0700

    Create Snip0241.py

---
+++ TrainingDataset/Snip0241.py
@@ -0,0 +1,9 @@
+def primitive_root(p_val: int) -> int:
+    print("Generating primitive root of p")
+    while True:
+        g = random.randrange(3, p_val)
+        if pow(g, 2, p_val) == 1:
+            continue
+        if pow(g, p_val, p_val) == 1:
+            continue
+        return g
