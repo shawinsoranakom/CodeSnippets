524ddf013fc8c8f877f37277993b09f49f40d1ea
Bossu284 <sirivej817@gmail.com>
2025-12-04 10:19:50 +0700

    Create Snip0230.py

---
+++ TrainingDataset/Snip0230.py
@@ -0,0 +1,14 @@
+def decrypt(message: str) -> None:
+   
+    for key in range(len(string.ascii_uppercase)):
+        translated = ""
+        for symbol in message:
+            if symbol in string.ascii_uppercase:
+                num = string.ascii_uppercase.find(symbol)
+                num = num - key
+                if num < 0:
+                    num = num + len(string.ascii_uppercase)
+                translated = translated + string.ascii_uppercase[num]
+            else:
+                translated = translated + symbol
+        print(f"Decryption using Key #{key}: {translated}")
