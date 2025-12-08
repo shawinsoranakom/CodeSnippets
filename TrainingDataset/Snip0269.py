9c85d6ef5ab9287da102c094e53eeab66789aabe
Bossu284 <sirivej817@gmail.com>
2025-12-08 23:26:32 +0700

    Create Snip0269.py

---
+++ TrainingDataset/Snip0269.py
@@ -0,0 +1,20 @@
+def prepare_input(dirty: str) -> str:
+
+    dirty = "".join([c.upper() for c in dirty if c in string.ascii_letters])
+    clean = ""
+
+    if len(dirty) < 2:
+        return dirty
+
+    for i in range(len(dirty) - 1):
+        clean += dirty[i]
+
+        if dirty[i] == dirty[i + 1]:
+            clean += "X"
+
+    clean += dirty[-1]
+
+    if len(clean) & 1:
+        clean += "X"
+
+    return clean
