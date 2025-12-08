75c21d00113550b7e71bec17e3570b9af558274e
Bossu284 <sirivej817@gmail.com>
2025-12-08 23:26:57 +0700

    Create Snip0270.py

---
+++ TrainingDataset/Snip0270.py
@@ -0,0 +1,13 @@
+def generate_table(key: str) -> list[str]:
+    alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
+    table = []
+
+    for char in key.upper():
+        if char not in table and char in alphabet:
+            table.append(char)
+
+    for char in alphabet:
+        if char not in table:
+            table.append(char)
+
+    return table
