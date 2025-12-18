0981d9b8bd7ad9393fbd9b460c44e823d5c25ddf
Bossu284 <sirivej817@gmail.com>
2025-12-18 14:03:15 +0700

    Create Eval0052.py

---
+++ EvaluationDataset/Eval0052.py
@@ -0,0 +1,4 @@
+def random_chars(number_char: int = 32) -> str:
+    assert number_char > 1, "The number of character should greater than 1"
+    letter_code = ascii_lowercase + digits
+    return "".join(random.choice(letter_code) for _ in range(number_char))
