a57df2392b678e84948b4fdebe968beb02cb594b
Bossu284 <sirivej817@gmail.com>
2025-12-26 19:07:41 +0700

    Create Eval0197.py

---
+++ EvaluationDataset/Eval0197.py
@@ -0,0 +1,11 @@
+def left_rotation(node: MyNode) -> MyNode:
+    print("right rotation node:", node.get_data())
+    ret = node.get_right()
+    assert ret is not None
+    node.set_right(ret.get_left())
+    ret.set_left(node)
+    h1 = my_max(get_height(node.get_right()), get_height(node.get_left())) + 1
+    node.set_height(h1)
+    h2 = my_max(get_height(ret.get_right()), get_height(ret.get_left())) + 1
+    ret.set_height(h2)
+    return ret
