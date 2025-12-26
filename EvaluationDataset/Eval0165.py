def5b61bae375bee242b7e71622337008cb6f10a
Bossu284 <sirivej817@gmail.com>
2025-12-26 18:58:23 +0700

    Create Eval0165.py

---
+++ EvaluationDataset/Eval0165.py
@@ -0,0 +1,10 @@
+def partition(arr: list[int], low: int, high: int) -> int:
+
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] >= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
