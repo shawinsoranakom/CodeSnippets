d0c46f50cc0b5ffc1fc085c75126ba561db6d124
Bossu284 <sirivej817@gmail.com>
2025-12-26 19:03:57 +0700

    Create Eval0184.py

---
+++ EvaluationDataset/Eval0184.py
@@ -0,0 +1,12 @@
+def display(values):
+    width = 1 + max(len(values[s]) for s in squares)
+    line = "+".join(["-" * (width * 3)] * 3)
+    for r in rows:
+        print(
+            "".join(
+                values[r + c].center(width) + ("|" if c in "36" else "") for c in cols
+            )
+        )
+        if r in "CF":
+            print(line)
+    print()
