ebb23f682cd10061bb722457bd198b6ea2da86d9
Bossu284 <sirivej817@gmail.com>
2025-12-26 18:40:09 +0700

    Create Eval0109.py

---
+++ EvaluationDataset/Eval0109.py
@@ -0,0 +1,32 @@
+class SIUnit(Enum):
+    yotta = 24
+    zetta = 21
+    exa = 18
+    peta = 15
+    tera = 12
+    giga = 9
+    mega = 6
+    kilo = 3
+    hecto = 2
+    deca = 1
+    deci = -1
+    centi = -2
+    milli = -3
+    micro = -6
+    nano = -9
+    pico = -12
+    femto = -15
+    atto = -18
+    zepto = -21
+    yocto = -24
+
+    @classmethod
+    def get_positive(cls) -> dict:
+
+        return {unit.name: unit.value for unit in cls if unit.value > 0}
+
+    @classmethod
+    def get_negative(cls) -> dict:
+
+        return {unit.name: unit.value for unit in cls if unit.value < 0}
+
