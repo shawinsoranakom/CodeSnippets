00beedbd2b7b795ef79461efab82101b2c5ab675
Bossu284 <sirivej817@gmail.com>
2025-12-26 18:49:08 +0700

    Create Eval0137.py

---
+++ EvaluationDataset/Eval0137.py
@@ -0,0 +1,8 @@
+class Letter:
+    def __init__(self, letter: str, freq: int):
+        self.letter: str = letter
+        self.freq: int = freq
+        self.bitstring: dict[str, str] = {}
+
+    def __repr__(self) -> str:
+        return f"{self.letter}:{self.freq}"
