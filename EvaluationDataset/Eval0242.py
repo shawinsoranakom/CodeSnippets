890fb941a06183df6eaa785fcb9e8df507a218e0
Bossu284 <sirivej817@gmail.com>
2025-12-26 23:14:16 +0700

    Create Eval0242.py

---
+++ EvaluationDataset/Eval0242.py
@@ -0,0 +1,8 @@
+def build_tree() -> TreeNode:
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(5)
+    root.left.left = TreeNode(3)
+    root.left.right = TreeNode(4)
+    root.right.right = TreeNode(6)
+    return root
