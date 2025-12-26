1c38d8101091e32401b428f7f034bf03b3980999
Bossu284 <sirivej817@gmail.com>
2025-12-26 23:40:03 +0700

    Create Eval0296.py

---
+++ EvaluationDataset/Eval0296.py
@@ -0,0 +1,9 @@
+def make_symmetric_tree() -> Node:
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(2)
+    root.left.left = Node(3)
+    root.left.right = Node(4)
+    root.right.left = Node(4)
+    root.right.right = Node(3)
+    return root
