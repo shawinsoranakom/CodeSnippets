2a7c6a046d5ed8b7e73aa3f779e37905e8c268e3
Bossu284 <sirivej817@gmail.com>
2025-12-26 23:20:10 +0700

    Create Eval0257.py

---
+++ EvaluationDataset/Eval0257.py
@@ -0,0 +1,8 @@
+def create_sparse(max_node: int, parent: list[list[int]]) -> list[list[int]]:
+    j = 1
+    while (1 << j) < max_node:
+        for i in range(1, max_node + 1):
+            parent[j][i] = parent[j - 1][parent[j - 1][i]]
+        j += 1
+    return parent
+
