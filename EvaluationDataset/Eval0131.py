c96277b7e1e6b72db30f3b934086a65928019d03
Bossu284 <sirivej817@gmail.com>
2025-12-26 18:46:52 +0700

    Create Eval0131.py

---
+++ EvaluationDataset/Eval0131.py
@@ -0,0 +1,17 @@
+def volume_conversion(value: float, from_type: str, to_type: str) -> float:
+
+    if from_type not in METRIC_CONVERSION:
+        raise ValueError(
+            f"Invalid 'from_type' value: {from_type!r}  Supported values are:\n"
+            + ", ".join(METRIC_CONVERSION)
+        )
+    if to_type not in METRIC_CONVERSION:
+        raise ValueError(
+            f"Invalid 'to_type' value: {to_type!r}.  Supported values are:\n"
+            + ", ".join(METRIC_CONVERSION)
+        )
+    return (
+        value
+        * METRIC_CONVERSION[from_type].from_factor
+        * METRIC_CONVERSION[to_type].to_factor
+    )
