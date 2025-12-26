18b0217eb6727c8012e9b88a279e72135957a6f5
Bossu284 <sirivej817@gmail.com>
2025-12-26 18:59:15 +0700

    Create Eval0168.py

---
+++ EvaluationDataset/Eval0168.py
@@ -0,0 +1,5 @@
+def is_monotonic(nums: list[int]) -> bool:
+
+    return all(nums[i] <= nums[i + 1] for i in range(len(nums) - 1)) or all(
+        nums[i] >= nums[i + 1] for i in range(len(nums) - 1)
+    )
