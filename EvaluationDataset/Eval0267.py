8feeb39fe34f4d9155934fd7cd3f8f88d2a0ffa2
Bossu284 <sirivej817@gmail.com>
2025-12-26 23:23:52 +0700

    Create Eval0267.py

---
+++ EvaluationDataset/Eval0267.py
@@ -0,0 +1,9 @@
+def make_tree_seven() -> Node:
+    tree = Node(1)
+    tree.left = Node(2)
+    tree.right = Node(3)
+    tree.left.left = Node(4)
+    tree.left.right = Node(5)
+    tree.right.left = Node(6)
+    tree.right.right = Node(7)
+    return tree
