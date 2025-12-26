000d976ab02718472abffa7aa4eaf448a920fd19
Bossu284 <sirivej817@gmail.com>
2025-12-26 19:09:21 +0700

    Create Eval0204.py

---
+++ EvaluationDataset/Eval0204.py
@@ -0,0 +1,54 @@
+class AVLtree:
+
+    def __init__(self) -> None:
+        self.root: MyNode | None = None
+
+    def get_height(self) -> int:
+        return get_height(self.root)
+
+    def insert(self, data: Any) -> None:
+        print("insert:" + str(data))
+        self.root = insert_node(self.root, data)
+
+    def del_node(self, data: Any) -> None:
+        print("delete:" + str(data))
+        if self.root is None:
+            print("Tree is empty!")
+            return
+        self.root = del_node(self.root, data)
+
+    def __str__(
+        self,
+    ) -> str:  
+        output = ""
+        q = MyQueue()
+        q.push(self.root)
+        layer = self.get_height()
+        if layer == 0:
+            return output
+        cnt = 0
+        while not q.is_empty():
+            node = q.pop()
+            space = " " * int(math.pow(2, layer - 1))
+            output += space
+            if node is None:
+                output += "*"
+                q.push(None)
+                q.push(None)
+            else:
+                output += str(node.get_data())
+                q.push(node.get_left())
+                q.push(node.get_right())
+            output += space
+            cnt = cnt + 1
+            for i in range(100):
+                if cnt == math.pow(2, i) - 1:
+                    layer = layer - 1
+                    if layer == 0:
+                        output += "\n*************************************"
+                        return output
+                    output += "\n"
+                    break
+        output += "\n*************************************"
+        return output
+
