5c3ce19cabbd821d61ce265d311034fc9cd949ea
Bossu284 <sirivej817@gmail.com>
2025-12-26 19:03:21 +0700

    Create Eval0182.py

---
+++ EvaluationDataset/Eval0182.py
@@ -0,0 +1,6 @@
+def assign(values, s, d):
+    other_values = values[s].replace(d, "")
+    if all(eliminate(values, s, d2) for d2 in other_values):
+        return values
+    else:
+        return False
