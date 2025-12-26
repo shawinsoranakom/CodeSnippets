33c3733ac480d2640186455e55b738be01ffce82
Bossu284 <sirivej817@gmail.com>
2025-12-26 19:08:03 +0700

    Create Eval0199.py

---
+++ EvaluationDataset/Eval0199.py
@@ -0,0 +1,5 @@
+def rl_rotation(node: MyNode) -> MyNode:
+    right_child = node.get_right()
+    assert right_child is not None
+    node.set_right(right_rotation(right_child))
+    return left_rotation(node)
