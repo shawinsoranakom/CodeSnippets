dd827733aa1569eaa8b32c307050b42b75a32268
Bossu284 <sirivej817@gmail.com>
2025-12-26 23:34:57 +0700

    Create Eval0286.py

---
+++ EvaluationDataset/Eval0286.py
@@ -0,0 +1,9 @@
+def test_tree_chaining() -> bool:
+    tree = RedBlackTree(0)
+    tree = tree.insert(-16).insert(16).insert(8).insert(24).insert(20).insert(22)
+    if list(tree.inorder_traverse()) != [-16, 0, 8, 16, 20, 22, 24]:
+        return False
+    if list(tree.preorder_traverse()) != [0, -16, 16, 8, 22, 20, 24]:
+        return False
+    return list(tree.postorder_traverse()) == [-16, 8, 20, 24, 22, 16, 0]
+
