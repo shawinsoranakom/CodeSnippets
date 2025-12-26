1e6eb0d328a6bab20d60aee87376211322e8ae6c
Bossu284 <sirivej817@gmail.com>
2025-12-26 23:34:13 +0700

    Create Eval0283.py

---
+++ EvaluationDataset/Eval0283.py
@@ -0,0 +1,13 @@
+def test_floor_ceil() -> bool:
+    tree = RedBlackTree(0)
+    tree.insert(-16)
+    tree.insert(16)
+    tree.insert(8)
+    tree.insert(24)
+    tree.insert(20)
+    tree.insert(22)
+    tuples = [(-20, None, -16), (-10, -16, 0), (8, 8, 8), (50, 24, None)]
+    for val, floor, ceil in tuples:
+        if tree.floor(val) != floor or tree.ceil(val) != ceil:
+            return False
+    return True
