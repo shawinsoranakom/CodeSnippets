140f6c680002ed730e072503a14b6b28940439b0
Bossu284 <sirivej817@gmail.com>
2026-01-08 14:23:17 +0700

    Create Eval0301.py

---
+++ TrainingDataset2/Eval0301.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, length: int) -> None:
+        self.minn: int = -1
+        self.maxx: int = -1
+        self.map_left: list[int] = [-1] * length
+        self.left: Node | None = None
+        self.right: Node | None = None
+
+    def __repr__(self) -> str:
+        
+        return f"Node(min_value={self.minn} max_value={self.maxx})"
