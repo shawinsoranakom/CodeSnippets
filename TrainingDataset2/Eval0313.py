b380124ce428e66e2c6b4005ac993572ae0b272c
Bossu284 <sirivej817@gmail.com>
2026-01-08 14:29:26 +0700

    Create Eval0313.py

---
+++ TrainingDataset2/Eval0313.py
@@ -0,0 +1,16 @@
+def test_disjoint_set() -> None:
+    vertex = [Node(i) for i in range(6)]
+    for v in vertex:
+        make_set(v)
+
+    union_set(vertex[0], vertex[1])
+    union_set(vertex[1], vertex[2])
+    union_set(vertex[3], vertex[4])
+    union_set(vertex[3], vertex[5])
+
+    for node0 in vertex:
+        for node1 in vertex:
+            if find_python_set(node0).isdisjoint(find_python_set(node1)):
+                assert find_set(node0) != find_set(node1)
+            else:
+                assert find_set(node0) == find_set(node1)
