cbb49e4a05d34e8b9099c253ea7f03134aa82247
Bossu284 <sirivej817@gmail.com>
2026-01-29 13:59:58 +0700

    Create Snip0454.py

---
+++ TrainingDataset3/Snip0454.py
@@ -0,0 +1,21 @@
+class QuadraticProbing(HashTable):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _collision_resolution(self, key, data=None): 
+        i = 1
+        new_key = self.hash_function(key + i * i)
+
+        while self.values[new_key] is not None and self.values[new_key] != key:
+            i += 1
+            new_key = (
+                self.hash_function(key + i * i)
+                if not self.balanced_factor() >= self.lim_charge
+                else None
+            )
+
+            if new_key is None:
+                break
+
+        return new_key
