55ea84960ca4d833fb864464b63907bb92a41afa
Bossu284 <sirivej817@gmail.com>
2026-01-29 14:28:52 +0700

    Create Snip0480.py

---
+++ TrainingDataset3/Snip0480.py
@@ -0,0 +1,15 @@
+class LinkedListIterator:
+    def __init__(self, head):
+        self.current = head
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if not self.current:
+            raise StopIteration
+        else:
+            value = self.current.data
+            self.current = self.current.next
+            return value
+
