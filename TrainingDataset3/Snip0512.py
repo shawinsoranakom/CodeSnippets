a04d1e6973417f31c2f0e4a9dba99a989d66e4f1
Bossu284 <sirivej817@gmail.com>
2026-01-29 15:16:28 +0700

    Create Snip0512.py

---
+++ TrainingDataset3/Snip0512.py
@@ -0,0 +1,31 @@
+def test_delete_removes_only_given_key():
+    skip_list = SkipList()
+
+    skip_list.insert("Key1", 12)
+    skip_list.insert("V", 13)
+    skip_list.insert("X", 14)
+    skip_list.insert("Key2", 15)
+
+    skip_list.delete("V")
+    assert skip_list.find("V") is None
+    assert skip_list.find("X") == 14
+    assert skip_list.find("Key1") == 12
+    assert skip_list.find("Key2") == 15
+
+    skip_list.delete("X")
+    assert skip_list.find("V") is None
+    assert skip_list.find("X") is None
+    assert skip_list.find("Key1") == 12
+    assert skip_list.find("Key2") == 15
+
+    skip_list.delete("Key1")
+    assert skip_list.find("V") is None
+    assert skip_list.find("X") is None
+    assert skip_list.find("Key1") is None
+    assert skip_list.find("Key2") == 15
+
+    skip_list.delete("Key2")
+    assert skip_list.find("V") is None
+    assert skip_list.find("X") is None
+    assert skip_list.find("Key1") is None
+    assert skip_list.find("Key2") is None
