e89ccac600bfeb958e7e43cebc75705556198c24
Bossu284 <sirivej817@gmail.com>
2026-01-29 15:15:35 +0700

    Create Snip0509.py

---
+++ TrainingDataset3/Snip0509.py
@@ -0,0 +1,14 @@
+def test_search():
+    skip_list = SkipList()
+
+    skip_list.insert("Key2", 20)
+    assert skip_list.find("Key2") == 20
+
+    skip_list.insert("Some Key", 10)
+    skip_list.insert("Key2", 8)
+    skip_list.insert("V", 13)
+
+    assert skip_list.find("Y") is None
+    assert skip_list.find("Key2") == 8
+    assert skip_list.find("Some Key") == 10
+    assert skip_list.find("V") == 13
