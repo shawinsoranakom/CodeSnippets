c2007b8fedf7cb32261edd1a25298e8aaf352dee
Bossu284 <sirivej817@gmail.com>
2026-01-29 14:18:23 +0700

    Create Snip0467.py

---
+++ TrainingDataset3/Snip0467.py
@@ -0,0 +1,18 @@
+def test_nearest_neighbour_search():
+    num_points = 10
+    cube_size = 10.0
+    num_dimensions = 2
+    points = hypercube_points(num_points, cube_size, num_dimensions)
+    kdtree = build_kdtree(points.tolist())
+
+    rng = np.random.default_rng()
+    query_point = rng.random(num_dimensions).tolist()
+
+    nearest_point, nearest_dist, nodes_visited = nearest_neighbour_search(
+        kdtree, query_point
+    )
+    assert nearest_point is not None
+
+    assert nearest_dist >= 0
+
+    assert nodes_visited >= 0
