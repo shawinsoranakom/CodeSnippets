3e515718ac3b330339f97814b7fb854f00aa770f
Bossu284 <sirivej817@gmail.com>
2026-02-05 14:12:36 +0700

    Update Snip0616.py

--- TrainingDataset4/Snip0616.py
+++ TrainingDataset4/Snip0616.py
@@ -1,2 +1,8 @@
-def __str__(self) -> str:
-    return f"{self.data}"
+def main() -> None:
+    root = RadixNode()
+    words = "banana bananas bandanas bandana band apple all beast".split()
+    root.insert_many(words)
+
+    print("Words:", words)
+    print("Tree:")
+    root.print_tree()
