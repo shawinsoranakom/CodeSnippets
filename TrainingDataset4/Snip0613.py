89ab374d174222a172f42f4fee6a69cc0b2be500
Bossu284 <sirivej817@gmail.com>
2026-02-05 14:13:54 +0700

    Update Snip0613.py

--- TrainingDataset4/Snip0613.py
+++ TrainingDataset4/Snip0613.py
@@ -1,3 +1,3 @@
-def __init__(self) -> None:
-        self.nodes: dict[str, TrieNode] = {}  
+    def __init__(self) -> None:
+        self.nodes: dict[str, TrieNode] = {}
         self.is_leaf = False
