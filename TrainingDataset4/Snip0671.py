e892286f671ef9b9559e9eb91b78283bb90652fd
Bossu284 <sirivej817@gmail.com>
2026-02-05 15:27:55 +0700

    Create Snip0671.py

---
+++ TrainingDataset4/Snip0671.py
@@ -0,0 +1,14 @@
+def dilation(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+    output = np.zeros_like(image)
+    image_padded = np.zeros(
+        (image.shape[0] + kernel.shape[0] - 1, image.shape[1] + kernel.shape[1] - 1)
+    )
+    image_padded[kernel.shape[0] - 2 : -1 :, kernel.shape[1] - 2 : -1 :] = image
+
+    for x in range(image.shape[1]):
+        for y in range(image.shape[0]):
+            summation = (
+                kernel * image_padded[y : y + kernel.shape[0], x : x + kernel.shape[1]]
+            ).sum()
+            output[y, x] = int(summation > 0)
+    return output
