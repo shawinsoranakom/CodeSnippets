ed78079d655bded6713d6ce8d6b5374cadb669b2
Bossu284 <sirivej817@gmail.com>
2026-02-05 15:07:28 +0700

    Create Snip0662.py

---
+++ TrainingDataset4/Snip0662.py
@@ -0,0 +1,5 @@
+def gen_gaussian_kernel(k_size, sigma):
+    center = k_size // 2
+    x, y = mgrid[0 - center : k_size - center, 0 - center : k_size - center]
+    g = 1 / (2 * pi * sigma) * exp(-(square(x) + square(y)) / (2 * square(sigma)))
+    return g
