dd85f9f36ef8e2d0da14b015212cd6a8fc86c641
Bossu284 <sirivej817@gmail.com>
2026-03-18 00:28:58 +0700

    Create Snip0895.py

---
+++ TrainingDataset5/Snip0895.py
@@ -0,0 +1,17 @@
+def dijkstra(graph, start, end):
+
+    heap = [(0, start)]
+    visited = set()
+    while heap:
+        (cost, u) = heapq.heappop(heap)
+        if u in visited:
+            continue
+        visited.add(u)
+        if u == end:
+            return cost
+        for v, c in graph[u]:
+            if v in visited:
+                continue
+            next_item = cost + c
+            heapq.heappush(heap, (next_item, v))
+    return -1
