ef0c23f79665033e60c914ab3d7f2e8f18b305cf
Bossu284 <sirivej817@gmail.com>
2026-03-17 23:15:41 +0700

    Create Snip0761.py

---
+++ TrainingDataset5/Snip0761.py
@@ -0,0 +1,11 @@
+def knapsack(w, wt, val, n):
+    dp = [[0] * (w + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for w_ in range(1, w + 1):
+            if wt[i - 1] <= w_:
+                dp[i][w_] = max(val[i - 1] + dp[i - 1][w_ - wt[i - 1]], dp[i - 1][w_])
+            else:
+                dp[i][w_] = dp[i - 1][w_]
+
+    return dp[n][w_], dp
