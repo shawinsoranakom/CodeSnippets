1ae7c71cdd4d9b87d0792b9052a342b112a2101a
Bossu284 <sirivej817@gmail.com>
2026-03-17 23:06:16 +0700

    Create Snip0748.py

---
+++ TrainingDataset5/Snip0748.py
@@ -0,0 +1,26 @@
+def merge(left_half: list, right_half: list) -> list:
+    sorted_array = [None] * (len(right_half) + len(left_half))
+
+    pointer1 = 0 
+    pointer2 = 0 
+    index = 0 
+    while pointer1 < len(left_half) and pointer2 < len(right_half):
+        if left_half[pointer1] < right_half[pointer2]:
+            sorted_array[index] = left_half[pointer1]
+            pointer1 += 1
+            index += 1
+        else:
+            sorted_array[index] = right_half[pointer2]
+            pointer2 += 1
+            index += 1
+    while pointer1 < len(left_half):
+        sorted_array[index] = left_half[pointer1]
+        pointer1 += 1
+        index += 1
+
+    while pointer2 < len(right_half):
+        sorted_array[index] = right_half[pointer2]
+        pointer2 += 1
+        index += 1
+
+    return sorted_array
