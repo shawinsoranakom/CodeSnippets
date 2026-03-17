d1b96260e969424cc7d7f50c727f2d0fb53bce54
Bossu284 <sirivej817@gmail.com>
2026-03-18 00:11:51 +0700

    Create Snip0859.py

---
+++ TrainingDataset5/Snip0859.py
@@ -0,0 +1,16 @@
+def prim(g, s):
+    dist, known, path = {s: 0}, set(), {s: 0}
+    while True:
+        if len(known) == len(g) - 1:
+            break
+        mini = 100000
+        for key, value in dist.items():
+            if key not in known and value < mini:
+                mini = value
+                u = key
+        known.add(u)
+        for v in g[u]:
+            if v[0] not in known and v[1] < dist.get(v[0], 100000):
+                dist[v[0]] = v[1]
+                path[v[0]] = u
+    return dist
