edd274289fab33a47d3832a2ad4b081f8d13142b
Bossu284 <sirivej817@gmail.com>
2026-03-17 23:08:13 +0700

    Create Snip0751.py

---
+++ TrainingDataset5/Snip0751.py
@@ -0,0 +1,13 @@
+def abbr(a: str, b: str) -> bool:
+    n = len(a)
+    m = len(b)
+    dp = [[False for _ in range(m + 1)] for _ in range(n + 1)]
+    dp[0][0] = True
+    for i in range(n):
+        for j in range(m + 1):
+            if dp[i][j]:
+                if j < m and a[i].upper() == b[j]:
+                    dp[i + 1][j + 1] = True
+                if a[i].islower():
+                    dp[i + 1][j] = True
+    return dp[n][m]
