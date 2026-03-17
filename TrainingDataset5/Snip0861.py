0f6126f8f39e676e73cee106ab27004832398704
Bossu284 <sirivej817@gmail.com>
2026-03-18 00:12:19 +0700

    Create Snip0861.py

---
+++ TrainingDataset5/Snip0861.py
@@ -0,0 +1,19 @@
+def krusk(e_and_n):
+    (e, n) = e_and_n
+    e.sort(reverse=True, key=lambda x: x[2])
+    s = [{i} for i in range(1, n + 1)]
+    while True:
+        if len(s) == 1:
+            break
+        print(s)
+        x = e.pop()
+        for i in range(len(s)):
+            if x[0] in s[i]:
+                break
+        for j in range(len(s)):
+            if x[1] in s[j]:
+                if i == j:
+                    break
+                s[j].update(s[i])
+                s.pop(i)
+                break
