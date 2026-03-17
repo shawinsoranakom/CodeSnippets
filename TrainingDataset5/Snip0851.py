a1ad3298aff1201e60294a55cb42561d1f6e734d
Bossu284 <sirivej817@gmail.com>
2026-03-18 00:09:38 +0700

    Create Snip0851.py

---
+++ TrainingDataset5/Snip0851.py
@@ -0,0 +1,12 @@
+def initialize_unweighted_undirected_graph(
+    node_count: int, edge_count: int
+) -> dict[int, list[int]]:
+    graph: dict[int, list[int]] = {}
+    for i in range(node_count):
+        graph[i + 1] = []
+
+    for e in range(edge_count):
+        x, y = (int(i) for i in _input(f"Edge {e + 1}: <node1> <node2> "))
+        graph[x].append(y)
+        graph[y].append(x)
+    return graph
